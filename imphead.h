ef4dc5b79547f7ba1bfbf0f81e6ff9b0edda46d4
Ayush Bhat <tfeayush@gmail.com>
2016-02-17 21:00:00 +0530

    Create imphead.h

---
+++ imphead.h
@@ -0,0 +1,25 @@
+// imphead.h( Important header files) 
+
+#include<iostream>
+#include<fstream>
+#include<sstream>
+#include<cmath>
+#include<cstdlib>
+#include<string>
+#include<list>
+#include<vector>
+#include<algorithm>
+#include<stdexcept>
+
+using namespace std;
+
+void error(string s)   //error function
+{
+    cout << s;
+}
+
+void keep_the_fucking_window_open()  // function to keep the window open
+{
+    char ch;
+    cin >> ch;
+}
